ffdb4fc02c598fe9e97d5bb784fa747e41f8f190
Crazelu <eberelucky64@gmail.com>
2021-04-25 00:43:44 +0100

    chore: adds dependencies, scaffolds HomeScreen

--- lib/ui/views/home.dart
+++ lib/ui/views/home.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:vaulty/ui/shared/responsive_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveWidget(builder: (context, size) {
-      return Container();
+      return Container(
+        height: size.height,
+        width: size.width,
+        child: ListView(
+          children: [
+            //search widget
+            Center(
+              child: Container(
+                height: 60.h,
+                decoration: BoxDecoration(),
+              ),
+            ),
+          ],
+        ),
+      );
     });
   }
 }
